ba595eadb7073f8c318cda7d7eb2bc10e8c6e3b6
David Doka <david@idopython.com>
2013-07-22 19:42:30 +0100

    travis-ci integration and adding runtests.sh

---
+++ runtests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Run Tests for Flaskengine
+
+
+cd tests/ 
+find . -name "*.pyc" -exec rm -rf {} \;
+clear && nosetests --logging-level=INFO  -s -x -v --without-sandbox --with-gae
+cd -
